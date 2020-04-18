f465c9644c013520731ce1db4edcb9cd678a2a67
Samira Ouraga <saouraga@microsoft.com>
2020-04-18 15:21:12 -0700

    Updates

---
+++ precache-manifest.f9f527a8d41cbcea0ccfd4fc9a4261ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cfc376558d4b3792c2ad366ff3ebe3",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "565a0a3f1a5c439d58df",
+    "url": "/Monsters-Rolodex/static/css/main.ef14d302.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/Monsters-Rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565a0a3f1a5c439d58df",
+    "url": "/Monsters-Rolodex/static/js/main.618d9715.chunk.js"
+  },
+  {
+    "revision": "321d78d19afe0ae88472",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.564099f5.js"
+  }
+]);
